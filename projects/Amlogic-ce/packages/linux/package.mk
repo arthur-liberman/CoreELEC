6827a916fe82afc4b47cafea16279ffa56869704
Arthur Liberman <arthur_liberman@hotmail.com>
2025-01-12 02:32:30 +0200

    linux:Amlogic-ng: bump package to 5ad4836e adjust linux.aarch64.conf to follow CoreELEC upstream add SMB-280 DTBs

--- projects/Amlogic-ce/packages/linux/package.mk
+++ projects/Amlogic-ce/packages/linux/package.mk
@@ -17,8 +17,8 @@ PKG_PATCH_DIRS="$LINUX"
 
 case "$LINUX" in
   amlogic-4.9)
-    PKG_VERSION="35abaaa771e177c95d1b8e418792f4010c094e46"
-    PKG_SHA256="772fa45893866fabfeeb5abcc777e0177f8287733a7314a2347ccdaebcd04e87"
+    PKG_VERSION="5ad4836e9c34a20ae8b64da126d70c5c0ee3ec3f"
+    PKG_SHA256="8c2d871078fb5d1c53777e46b79ccf44ebaac868fbb28d3138696a7d9132c98c"
     PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
